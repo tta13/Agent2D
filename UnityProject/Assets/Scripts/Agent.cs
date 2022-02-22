257c4288df1a244a05dc24f6725e60358f341da2
tta13 <tta@cin.ufpe.br>
2022-02-21 22:35:36 -0300

    Added agent script implementation

--- UnityProject/Assets/Scripts/Agent.cs
+++ UnityProject/Assets/Scripts/Agent.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Agent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    [SerializeField] private Text text;
+
+    private Transform goal;
+    private int count;
+
+    private void Start()
     {
-        
+        count = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move(goal.position - transform.position);
+    }
+
+    private void Move(Vector2 direction)
+    {
+        direction = direction.normalized;
+        Vector2 position = transform.position;
+        position += direction * (speed * Time.fixedDeltaTime);
+        transform.position = position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var foodComponent = collision.GetComponent<Food>();
+        if (foodComponent)
+        {
+            Debug.Log("Delicious :)");
+            count++;
+            text.text = $"Néctares coletados: {count}";
+            Destroy(foodComponent.gameObject);
+        }
+    }
+
+    public void OnPerceiveFood(GameObject food)
+    {
+        goal = food.transform;
     }
 }
--- UnityProject/Assets/Scripts/FoodGenerator.cs
+++ UnityProject/Assets/Scripts/FoodGenerator.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FoodGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject foodPrefab;
     [SerializeField] private float minX, maxX, minY, maxY;
+    public GenerateFoodEvent onGenerateFood;
 
     private void Start()
     {
@@ -16,6 +18,10 @@ public class FoodGenerator : MonoBehaviour
         var randomY = Random.Range(minY, maxY);
         var food = Instantiate(foodPrefab, new Vector2(randomX, randomY), Quaternion.identity);
         var foodComponent = food.GetComponent<Food>();
+        onGenerateFood.Invoke(food);
         foodComponent.onDestroy.AddListener(GenerateFood);
     }
 }
+
+[System.Serializable]
+public class GenerateFoodEvent : UnityEvent<GameObject> { }
