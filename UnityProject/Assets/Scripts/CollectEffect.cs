fb19dade229f68b0e961a8718c4f2ffa694893e7
tta13 <tta@cin.ufpe.br>
2022-02-22 13:17:30 -0300

    Added particle effect to use when food is eaten

---
+++ UnityProject/Assets/Scripts/CollectEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entities.Interactables.Collectible
+{
+    public class CollectEffect : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
+
